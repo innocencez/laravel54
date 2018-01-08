273e5867cb82c4b4cc1c9ff9a3d41c37e44d69a6
innocence <645328751@qq.com>
2018-01-08 23:04:22 +0800

    20180108 更新

--- routes/admin.php
+++ routes/admin.php
@@ -10,5 +10,10 @@ Route::group(['prefix'=>'admin'],function (){
     Route::group(['middleware'=>'auth:admin'],function (){
         //后台首页
         Route::get('/home','\App\Admin\Controllers\HomeController@index');
+        //用户列表页
+        Route::get('/users','\App\Admin\Controllers\UserController@index');
+        //添加用户
+        Route::get('/users/create','\App\Admin\Controllers\UserController@create');
+        Route::post('/users/store','\App\Admin\Controllers\UserController@store');
     });
 });
